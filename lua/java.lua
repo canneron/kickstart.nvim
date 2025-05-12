de70f4b179e04bc059f3c30502a6e812f664b179
canneron <mcgreevycameron@gmail.com>
2025-05-12 15:38:24 +0100

    Adding java lsp

--- lua/java.lua
+++ lua/java.lua
@@ -1,9 +1,28 @@
-local lspconfig = require("lspconfig")
+local lspconfig = require 'lspconfig'
 
-lspconfig.jdtls.setup {
-  capabilities = capabilities,
-  on_attach = on_attach,
-  root_dir = lspconfig.util.root_pattern("pom.xml", ".git"),
-  cmd = { "jdtls" },
-  filetypes = { "java" },
+local home = os.getenv 'HOME'
+local jdtls_path = home .. '/.local/share/jdtls/1.19.0'
+
+require('jdtls').start_or_attach {
+  cmd = {
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-jar',
+    vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+    '-configuration',
+    jdtls_path .. '/config_linux',
+    '-data',
+    vim.fn.stdpath 'data' .. '/site/java/workspace/',
+  },
+  root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew' },
 }
